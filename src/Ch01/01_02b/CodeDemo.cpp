c195f051444418c3eaecc0e6b55a453d3168e5e3
Pavel-Martinez-22 <140538827+Pavel-Martinez-22@users.noreply.github.com>
2025-06-12 03:02:29 +0000

    Updated Code

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,13 @@
 // Complete Guide to C++ Programming Foundations
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Hi There" << endl;
+    cout << endl << endl;
+    return 0;
+}
