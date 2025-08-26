10f7110bce02182b6818590163f23374cdf28909
Pavel-Martinez-22 <140538827+Pavel-Martinez-22@users.noreply.github.com>
2025-08-26 19:24:45 +0000

    Finished Ch04_04b

--- src/Ch04/04_04b/CodeDemo.cpp
+++ src/Ch04/04_04b/CodeDemo.cpp
@@ -4,28 +4,35 @@
 
 #include <iostream>
 #include <string>
-#include <chrono>
+#include <chrono> //This library contains classes for dealing with time
 
 // Type aliases for chrono classes
+//These aliases make the code more readable by providing shorter and more meaningful names
 using Clock = std::chrono::system_clock;
 using TimePoint = std::chrono::time_point<Clock>;
 using Duration = std::chrono::duration<double>;
 
 int main(){
-    TimePoint start, end;
-    Duration elapsed_seconds;
+    TimePoint *start, *end; //Pointers start and end point TimePoint start object and TimePoint end object respectively
+    Duration *elapsed_seconds = new Duration(); // elapsed_second Pointer to a dynamically allocated Duration object on the heap
+
     std::string input;
 
-    start = Clock::now();
+    start = new TimePoint(Clock::now()); // start pointer is initialized to point to a Dynamically created TimePoint object created on the heap with current time
 
     std::cout << "Type 'start' as quickly as you can and hit ENTER: " << std::flush;
     std::cin >> input;
 
-    end = Clock::now();
-    elapsed_seconds = end - start;
+    end = new TimePoint(Clock::now());// end pointer  is initialized to point to a Dynamically created TimePoint object created on the heap with current time
+    *elapsed_seconds = *end - *start; // Calculate elapsed time by subtracting start time object from end time object and store the result in the Duration object pointed to by elapsed_seconds (by dereferencing the pointer)
 
-    std::cout << "Reaction time: " << elapsed_seconds.count() << "s" << std::endl;
+    std::cout << "Reaction time: " << elapsed_seconds->count() << "s" << std::endl;
     
+    //Delete objects to free memory, this will avoid memory leakage
+    delete start;
+    delete end;
+    delete elapsed_seconds;
+
     std::cout << std::endl << std::endl;
     return 0;
 }
