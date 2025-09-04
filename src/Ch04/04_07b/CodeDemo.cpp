352e622d0a5cd771790bac26563d146b40371247
Pavel-Martinez-22 <140538827+Pavel-Martinez-22@users.noreply.github.com>
2025-09-04 20:57:35 +0000

    Finished Examples

--- src/Ch04/04_07b/CodeDemo.cpp
+++ src/Ch04/04_07b/CodeDemo.cpp
@@ -7,7 +7,19 @@
 #include <complex>
 
 int main(){
-    
+    std::vector<std::complex<double>> points;
+
+    points.push_back(std::complex<double>(3.5, 4.0));
+    points.push_back(std::complex<double>(1.0, -2.1));
+    points.push_back(std::complex<double>(-5.3, 6.0));
+    points.push_back(std::complex<double>(7.2, 8.9));  
+
+    // Accessing elements in different ways
+    std::cout << "The first real part: " << points.begin()->real() << std::endl;//begin() returns an iterator to the first element, we then use the arrow operator to access the real member function which returns the real part of the complex number.
+    std::cout << "Imaginary part at index 1: " << points[1].imag() << std::endl;//We access this by index, and then use the dot operator to access the imag member function which returns the imaginary part of the complex number.
+    std::cout << "Next to last real: " << prev(points.end(), 2)->real() << std::endl;//prev() this guarantees going back a number of positions in the container. Argument 1 is the iterator and the 2nd argument is the number of positions you want to go back. we then use the arrow operator to access the real member function which returns the real part of the complex number.
+    std::cout << "Last imaginary: " << (points.end() - 1)->imag() << std::endl; //end() returns an iterator to one past the last element, so we decrement it by one to get the last element. we then use the arrow operator to access the imag member function which returns the imaginary part of the complex number.
+
     std::cout << std::endl << std::endl;
     return 0;
 }
--- src/Ch04/04_09b/CodeDemo.cpp
+++ src/Ch04/04_09b/CodeDemo.cpp
@@ -7,13 +7,27 @@
 
 int main(){
     const size_t LENGTH = 50;
-    char racer1[];
+    char racer1[] = "Speedy";//C string using a string literal character array
     char racer2[LENGTH];
     char raceResult[LENGTH * 2];
+
+    //Using Function strcpy to copy a string into a buffer
+    strncpy(racer2, "Lightning", sizeof(racer2) - 1); 
+    racer2[sizeof(racer2) - 1] = '\0'; //Null-terminate the string racer2
 
     std::cout << "Racer 1: " << racer1 << std::endl;
     std::cout << "Racer 2: " << racer2 << std::endl;
 
+    //Using Function strcpy to copy a string into a buffer
+    strncpy(raceResult, "And the winner is ", sizeof(raceResult) - 1);
+    raceResult[sizeof(raceResult) - 1] = '\0'; //Null-terminate the string raceResult
+
+    //Using Function strcat to concatenate two strings
+    strncat(raceResult, racer1, sizeof(raceResult) - strlen(raceResult) - 1);
+    
+    std::cout << raceResult << std::endl;
+    
     std::cout << std::endl << std::endl;
+
     return 0;
 }
